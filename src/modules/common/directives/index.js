9eb215b2bff89149b77bd7970b7b1567a27227b1
Jon Tan <joanthan.tan83@gmail.com>
2015-11-11 01:09:36 -1000

    initial commit

---
+++ src/modules/common/directives/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports =
+  angular.module('clearancefly.common.directives', []);
+  //.directive('fooDirective', require('./fooDirective'));
---
+++ src/modules/common/services/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports =
+  angular.module('clearancefly.common.services', []);
+  //.factory('fooService', require('./fooService'));
